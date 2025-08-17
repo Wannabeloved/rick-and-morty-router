4c9c50c067596275887c4e3bbd1acaa7fa261e79
Artemy <quzyboy@gmail.com>
2025-08-17 13:23:56 +0300

    feat(components): add `AuthContext` Provider into `Layout`

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,16 +2,19 @@
 import { Outlet } from 'react-router';
 import { NavigationBar } from './NavigationBar';
 import { PreviousPathProvider } from '../context/PreviousPathContext';
+import { AuthProvider } from '../context/AuthContext.tsx';
 
 export const Layout = () => {
   return (
-    <PreviousPathProvider>
-      <div className="min-h-screen bg-gray-900 text-white">
-        <NavigationBar />
-        <main className="container mx-auto p-4">
-          <Outlet />
-        </main>
-      </div>
-    </PreviousPathProvider>
+    <AuthProvider>
+      <PreviousPathProvider>
+        <div className="min-h-screen bg-gray-900 text-white">
+          <NavigationBar />
+          <main className="container mx-auto p-4">
+            <Outlet />
+          </main>
+        </div>
+      </PreviousPathProvider>
+    </AuthProvider>
   );
 };
