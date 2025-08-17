5a5dd647097e9f83faabd2c5881af7566693481e
Artemy <quzyboy@gmail.com>
2025-08-17 13:36:15 +0300

    feat(components): add auth buttons to `NavigationBar`

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,5 +1,6 @@
 
 import { NavLink } from 'react-router';
+import { useAuth } from '../context/AuthContext';
 
 const navLinks = [
   { path: '/', label: 'Home' },
@@ -8,7 +9,8 @@ const navLinks = [
   { path: '/episodes', label: 'Episodes' },
 ];
 
-export const NavigationBar = () => {
+export function NavigationBar() {
+
   return (
     <nav className="bg-gray-800 text-white p-4 shadow-md">
       <ul className="flex justify-center space-x-6">
@@ -28,7 +30,48 @@ export const NavigationBar = () => {
             </NavLink>
           </li>
         ))}
+        <AuthButtons />
       </ul>
     </nav>
   );
 };
+
+function AuthButtons() {
+  const { isLoggedIn, logout } = useAuth();
+
+  const buttonClasses = "bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded-full transition-transform transform hover:scale-105";
+
+  return (
+    <div className="flex space-x-4">
+      {isLoggedIn ? (
+          <li>
+            <button
+              onClick={logout}
+              className={buttonClasses}
+            >
+              Signout
+            </button>
+          </li>
+        ) : (
+          <>
+            <li>
+              <NavLink
+                to="/login"
+                className={buttonClasses}
+              >
+                Login
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/signup"
+                className={buttonClasses}
+              >
+                Signup
+              </NavLink>
+            </li>
+          </>
+        )}
+    </div>
+  );
+}
