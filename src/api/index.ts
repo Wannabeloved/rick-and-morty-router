918ab244733ab5554e1673c29fee41deef5f20d6
Artemy <quzyboy@gmail.com>
2025-08-23 14:06:50 +0300

    feat(api): Update API and types for pagination

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,29 +1,34 @@
 
-import type { Category, Item } from '../types';
+import { isUrlString, type Category, type Item, type PaginatedResponse, type UrlString } from '../types';
 
 const API_BASE_URL = 'https://rickandmortyapi.com/api';
 
-const categoryToEndpoint: Record<Category, string> = {
-  characters: 'character',
-  locations: 'location',
-  episodes: 'episode',
-};
+export const getItems = async <T extends Category>(
+  category: T,
+  page?: UrlString | number,
+): Promise<PaginatedResponse<T>> => {
+  let url;
+  
+  if (typeof page === "number")
+    url = `${API_BASE_URL}/${category}?page=${page}`;
+  else if (isUrlString(page))
+    url = page;
+  else
+    url = `${API_BASE_URL}/${category}`;
 
-export const getItems = async (category: Category): Promise<Item[]> => {
-  const endpoint = categoryToEndpoint[category];
-  const response = await fetch(`${API_BASE_URL}/${endpoint}`);
+  const response = await fetch(url);
+
   if (!response.ok)
     throw new Error(`Failed to fetch ${category}`);
 
-  const data = await response.json();
-  return data.results;
+  return await response.json();
 };
 
 export const getItem = async (category: Category, id: string): Promise<Item> => {
-  const endpoint = categoryToEndpoint[category];
+  const endpoint = category;
   const response = await fetch(`${API_BASE_URL}/${endpoint}/${id}`);
-  if (!response.ok)
+  if (!response.ok) {
     throw new Error(`Failed to fetch ${category} with id ${id}`);
-  
+  }
   return response.json();
 };
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,27 +1,71 @@
 
-export interface Character {
-  id: number;
-  name: string;
-  status: string;
-  species: string;
-  gender: string;
-  image: string;
+export interface PaginatedResponse<T extends (Category | Item) = Item> {
+  info: Info;
+  results: T extends Category ? Item<T>[] : T[];
 }
 
-export interface Location {
-  id: number;
-  name: string;
-  type: string;
-  dimension: string;
+export type Category = keyof typeof CategoryMap;
+
+export type Item<T extends Category = Category> 
+  = (typeof CategoryMap)[T];
+
+export interface Info {
+  count: number;
+  pages: number;
+  next: UrlString | null;
+  prev: UrlString | null;
+}
+const CategoryMap = {
+  character:{} as {
+    id: number;
+    name: string;
+    status: string;
+    species: string;
+    type: string;
+    gender: string;
+    origin: {
+      name: string;
+      url: string;
+    };
+    location: {
+      name: string;
+      url: string;
+    };
+    image: string;
+    episode: string[];
+    url: string;
+    created: string;
+  },
+  location:{} as {
+    id: number;
+    name: string;
+    type: string;
+    dimension: string;
+    residents: string[];
+    url: string;
+    created: string;
+  },
+  episode:{} as {
+    id: number;
+    name: string;
+    air_date: string;
+    episode: string;
+    characters: string[];
+    url: string;
+    created: string;
+  }
+}
+export const CATEGORIES: Category[] = Object.keys(CategoryMap) as Category[];
+
+export type UrlString = string & { __brand: "UrlString" };
+export const asUrlString = (value: string): UrlString => 
+  value as UrlString;
+export function isUrlString(value: unknown): value is UrlString {
+  try {
+    new URL(value as string);
+    return true;
+  } catch (error) {
+    return false;
+  }
 }
 
-export interface Episode {
-  id: number;
-  name: string;
-  air_date: string;
-  episode: string;
-}
-
-export type Category = 'characters' | 'locations' | 'episodes';
-
-export type Item = Character | Location | Episode;
