23ab728fb656d80042fcde25f5f43566d4110bc1
Artemy <quzyboy@gmail.com>
2025-09-01 00:47:28 +0300

    feat(pages): update HomePage with author info and new design

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,29 +2,42 @@
 import { Button } from '@heroui/button';
 import { Link } from 'react-router';
 // import { Link } from '@heroui/link';
+import { Avatar, AvatarIcon } from '@heroui/avatar';
+import { Image } from '@heroui/image';
+import { Spacer } from '@heroui/spacer';
+import { useAuthorInfo } from '../context/AuthorContext';
 
+const AuthorAvatar = () => {
+  const info = useAuthorInfo();
+  return (
+    <Avatar 
+      src={info.avatar_url}
+      name={info.name}
+      as="a"
+      href={info.html_url}
+      // fallback={<AvatarIcon />}
+      className="w-30 h-30 text-large"
+    />
+  );
+};
 export const HomePage = () => {
   return (
     <div className="flex flex-col items-center justify-center h-[calc(100vh-150px)] text-center p-4">
-      <h1 className="text-5xl md:text-7xl font-extrabold text-white mb-4">
-        <span className="bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500">
-          Rick and Morty
-        </span>
-        <span className="block text-4xl md:text-5xl mt-2">Universe Explorer</span>
+      <h1 className="flex text-5xl md:text-7xl font-extrabold text-white mb-4 items-center">
+        <AuthorAvatar />
+        <Spacer x={3}/>
+        X
+        <Spacer x={3}/>
+        <Image 
+          src="https://upload.wikimedia.org/wikipedia/commons/archive/b/b1/20220319060843%21Rick_and_Morty.svg"
+          className="object-cover"
+          height={100}
+        />
       </h1>
       <p className="text-lg text-gray-300 max-w-2xl mx-auto mb-8">
         Dive into the multiverse. Explore detailed information about all the characters, locations, and episodes from the show.
       </p>
       <div className="mb-3 flex flex-wrap justify-center gap-4">
-        {/* <Link to="/characters" className="bg-green-500 hover:bg-green-600 text-white font-bold py-3 px-6 rounded-full transition-transform transform hover:scale-105">
-          Explore Characters
-        </Link>
-        <Link to="/locations" className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-3 px-6 rounded-full transition-transform transform hover:scale-105">
-          Discover Locations
-        </Link>
-        <Link to="/episodes" className="bg-purple-500 hover:bg-purple-600 text-white font-bold py-3 px-6 rounded-full transition-transform transform hover:scale-105">
-          View Episodes
-        </Link> */}
         <Button 
           as={Link}
           to="/characters"
