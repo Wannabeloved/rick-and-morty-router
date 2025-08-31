be19df85c9def1bbfd61ce7d2ef51e36b3bba7bf
Artemy <quzyboy@gmail.com>
2025-08-31 21:43:20 +0300

    feat(components): refactor Spinner with heroui component

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,7 +1,9 @@
+import { Spinner as HeroSpinner } from "@heroui/spinner";
+
 export const Spinner = () => {
   return (
     <div className="flex justify-center items-center h-32">
-      <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-yellow-500"></div>
+      <HeroSpinner size="lg" />
       <p className="ml-4 text-xl text-gray-400">Loading...</p>
     </div>
   );
